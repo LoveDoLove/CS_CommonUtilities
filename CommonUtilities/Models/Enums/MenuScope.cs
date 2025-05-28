de4ab5484be32c11bedbf20f7e67bff2a069f79e
LoveDoLove <v0130p5100cuboss@gmail.com>
2025-05-28 14:11:49 +0800

    Release 1.4.1

---
+++ CommonUtilities/Models/Enums/MenuScope.cs
@@ -0,0 +1,17 @@
+namespace CommonUtilities.Models.Enums
+{
+    /// <summary>
+    /// Specifies the scope for a context menu entry.
+    /// </summary>
+    public enum MenuScope
+    {
+        /// <summary>
+        /// The context menu entry is for the current user.
+        /// </summary>
+        User,
+        /// <summary>
+        /// The context menu entry is for all users on the system.
+        /// </summary>
+        System
+    }
+}
